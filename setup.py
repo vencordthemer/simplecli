37633e435d0a38fddf2910f9a4499075e9d9612e
discordthemes <sampelter183@gmail.com>
2025-03-26 17:28:18 +0000

    Initial commit of climenus package

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 from setuptools import setup, find_packages
 
+# Read the long description from README.md
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="simplecli",
+    name="climenus",  # Your new package name
     version="0.1.0",
-    author="vencordthemer",
+    author="VencordThemer",
+    author_email="vencordthemer@users.noreply.github.com",
     description="A simple framework for building interactive CLI menus in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/interactivecli",
-    packages=find_packages(),
+    url="https://github.com/vencordthemer/simplecli",
+    project_urls={
+        "Bug Tracker": "https://github.com/vencordthemer/simplecli/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
     ],
+    keywords="cli, menu, terminal, console, interactive",
+    packages=find_packages(),
     python_requires=">=3.6",
 )
+
--- simplecli/__init__.py
+++
@@ -1,4 +0,0 @@
-from .menu import Menu
-from .utils import clear_screen, create_simple_menu, prompt, confirm
-
-__version__ = "0.1.0"
