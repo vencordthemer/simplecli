31040e720db99bdeb432df66c412898066a3f378
discordthemes <sampelter183@gmail.com>
2025-04-11 08:26:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="climenus",  # Your new package name
+    name="climenus",  
     version="0.1.0",
     author="VencordThemer",
     author_email="vencordthemer@users.noreply.github.com",
